4db63c161dbffae2d98ba40d9a9d0f5a510fc549
Artem <>
2025-04-19 19:41:31 +0700

    + version bump + update gradle + update kotlin

--- app/src/main/java/ru/wearemad/app/MainActivity.kt
+++ app/src/main/java/ru/wearemad/app/MainActivity.kt
@@ -1,7 +1,8 @@
 package ru.wearemad.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.wearemad.utils.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
